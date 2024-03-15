80e245c86affc4078c5cdaea500bdefe1c9462de
yelinkimm <kiylin94@gmail.com>
2024-03-15 22:50:53 +0900

    Add price tab

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -38,7 +38,7 @@ interface InfoData {
   last_data_at: string;
 }
 
-interface PriceData {
+export interface PriceData {
   id: string;
   name: string;
   symbol: string;
@@ -252,7 +252,7 @@ function Coin() {
 
           <Switch>
             <Route path={`/:coinId/price`}>
-              <Price/>
+              <Price tickersData={tickersData}/>
             </Route>
             <Route path={`/:coinId/chart`}>
               <Chart coinId={coinId}/>
--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,6 +1,58 @@
-function Price() {
+import styled from "styled-components";
+import { PriceData } from "./Coin";
+import moment from "moment";
+
+interface IPriceProps {
+  tickersData?: PriceData
+}
+
+function Price({ tickersData }: IPriceProps) {
+  const PriceItemContainer = styled.div`
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+  `;
+
+  const PriceItem = styled.div`
+    display: flex;
+    justify-content: space-between;
+  `;
+
   return (
-    <h1>Price</h1>
+    <div>
+      <PriceItemContainer>
+        <PriceItem>
+          <span>Price:</span>
+          <span>$ {tickersData?.quotes?.USD?.price}</span>
+        </PriceItem>
+      </PriceItemContainer>
+
+      <PriceItemContainer>
+        <PriceItem>
+          <span>Total Volume 24h:</span>
+          <span>$ {tickersData?.quotes?.USD?.volume_24h}</span>
+        </PriceItem>
+        <PriceItem>
+          <span>% from ATH:</span>
+          <span>{tickersData?.quotes?.USD?.percent_from_price_ath}%</span>
+        </PriceItem>
+      </PriceItemContainer>
+
+      <PriceItemContainer>
+        <PriceItem>
+          <span>Volume ATH:</span>
+          <span>$ {tickersData?.quotes?.USD?.ath_price}</span>
+        </PriceItem>
+        <PriceItem>
+          <span>Volume ATH Date:</span>
+          <span>
+            {moment(tickersData?.quotes?.USD?.ath_date).format("YYYY-MM-DD")}
+            {/* ({moment(tickersData?.quotes?.USD?.ath_date).fromNow()}) */}
+          </span>
+        </PriceItem>
+      </PriceItemContainer>
+    </div>
   )
 }
 
