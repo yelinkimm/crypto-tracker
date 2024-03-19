ba1715a8e876e1d0d2611b411705c626a944eef3
Iylin Kim <kiylin94@gmail.com>
2024-03-19 15:17:55 +0900

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Router from "./Router";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
-import { useState } from "react";
 import ThemeBtn from "./components/ThemeBtn";
 import { useRecoilValue } from "recoil";
 import { isDarkAtom } from "./atoms";
